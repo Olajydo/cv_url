73b8d728279fa901241be588060a5fc9a8346af2
Yusuf Olajide <100318861+Olajydo@users.noreply.github.com>
2023-05-22 10:45:10 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -11,9 +11,8 @@ st.image(image, width=250)
 
 st.markdown('## Summary', unsafe_allow_html = True)
 st.info("""
-- Experience Data Scientist and affable team leader who picks things up quickly. You can do anything if you have grit and a growing attitude.
-- Mathematician and a critical thinker demonstrated in various projects and remote internships with leading tech companies which has given me the opportunity to use my skill to provide business solutions.
-- Yusuf Olajide is passionate about having a positive impact on the world.Storytelling through data-driven insights with a focus on the aim.
+Lover of basketball; critical thinker. passionate about using data science, creativity, and problem-solving to make a difference in the real world.
+I have the ability to use my data skill and zealous actions to provide solutions that foster growth in the business fields in which I have had the privilege to work. However, my abilities are not limited to those fields; I am also a quick learner, personable leader with a growth mindset, a great team player, and a good listener. I am competent and can quickly get along with others, present insights, and communicate with a corporate body or individual using my data science skill.
 
 """)
 
@@ -58,42 +57,37 @@ st.write("""
 
 txt2('**Loreon Investment**, Intern, Data Science', 'February, 2022 – July, 2022')
 st.info("""
-- I provided innovative analytical insights in line with business objectives and conduct detailed data analysis on data used across the business units to evaluate business processes and improve business performance tracking
-- I identify, communicate, and endeavor to resolve data gaps that impact the fulfillment of the business functional requirements, by identifying, evaluating, and documenting potential data sources in support of project requirements within the assigned departments in the business.
-- Collected, cleaned and preprocessed over 500 rows of sales tickets data from 21 betting outlet daily which enabled proper documentation and tracking of sales KPIs for business growth.
-- Used Microsoft Excel functions & formulas to report sales activities from 21 betting outlet of the business daily, enabling management allocate resources appropriately which enabled effective distribution of company resources and reduced operation
-- Utilized Data Quality Assessment Framework to pre-process data. Examined and gathered 
-
+- Using the MT5 API, I pulled the live data for various time intervals to analyze and share insights on how to apply a strategy for better trading success.
+- Proper documentation and tracking of sales KPIs for business growth were made possible by data that had been collected, cleansed, and preprocessed.
+- utilized Python programming to create Bollinger Bands, support & resistance, and SMA for tactical trading.
+- With this approach created from these several indications, automation was set up to be used frequently for better and more precise Trading.
 
 """)
 
 
 txt2('**BCG, Forage**, Data Science and Advance analytics virtual experience program', 'July, 2022 - September, 2022' )
 st.info("""
-- I provided business innovative ideas to help resolve the customer churn rate 
-- I identify and provided the hypothesis framing for churn customer
-- I explored the dataset to identify useful insight to understand customers attitudinal features through visualization
-- With machine learning I made model to predict for churn customers
-- Communicate the interpretation of the model and help identify the important features which will help to reduce and identify customer that is liable to churn and measures to take 
+- I identified and offered the hypothesis framing for turnover customer in my firm unique ideas to help reduce the customer churn rate.
+- By analyzing the data, insightful information was that was used to explain to the corporate body through visualization how customers' attitudinal characteristics.
+- With the aid of machine learning, I created a model to anticipate customer churn, which helped give the optimal business plan for client retention by giving usable insight. 
 
 """)
 
 txt2('**British Airways**, Data Science and Advance analytics virtual experience program', 'October 2022 - November, 2022')
 st.info("""
-- I performed some web scraping using the Python library (Beautiful Soup), which allowed me to collect customer reviews from British airline pages.
-- I also performed some text cleaning on the data that appeared messy to prevent it from interfering with the model's results. Also, I performed topic modeling in order to comprehend and identify the subject that is frequently discussed on British Airways websites.
-- Performed Sentiment analysis to identify and categorize reviews in order to know customers attitude towards British airways
-- Presented my model interpretation with a Microsoft PowerPoint presentation available to stakeholders to help them identify important customer review-related actions 
-- Built a predictive model with Machine learning to understand factors influencing customer buying behaviors
-- Interpreting and presenting my models and analysis to key stakeholders which help to provide actionable insights to business actions
+-  I used web scraping to get customer reviews from British Airways site using the Beautiful Soup Python package.
+-  In order to prevent it from affecting the model's findings, I also cleaned up the text in the data that appeared disorganized. Additionally, I used topic modeling to understand and recognize the issue that was commonly addressed on British Airways websites.
+-  Conducted sentiment analysis to find and analyze reviews in order to understand how people feel about British Airways
+-  I provided a Microsoft PowerPoint presentation to stakeholders that included my model interpretation to assist them in recognizing crucial customer review-related activities.
+-  developing a predictive model to comprehend the aspects impacting client purchasing decisions, which was used to provide business intelligence solutions to resolve company problems and provide a positive customer experience.
 
 """)
-st.info(""" Experience with using Api to fetch data from numerous sites:
+st.info(""" experience retrieving data from a number of websites via the API :
 - With the use of rapidAPI i pulled data from linkedin to get the list of data analyst jobs available
 - also use the Zoopla API to fetch the list of available properties for rent and sales in Oxoford, including the running cost of each property
 """)
 st.write("""## Education and Certifications""")
-txt('**Bachelor of Technology**, (Pure and Applied Mathematics), Federal University of Technology, Minna, Niger State,Nigeria.','2015-2022')
+txt('**B.Tech**, (Pure and Applied Mathematics), Federal University of Technology, Minna, Niger State,Nigeria.','2015-2022')
 txt('**Kaggle** Data science Certifications', ' June, 2022') 
 txt('**SkillUP** Data science with Python', 'September, 2022')
 txt('**SkillUP** Power BI for Beginners', 'October, 2022')
@@ -103,7 +97,7 @@ st.write("""
 ## Skills and Tool
 """) 
 txt('**Programming**', "Python" )
-txt('**Data processing/wrangling**', '"Sql", "Pandas"')
+txt('**Data processing/wrangling**', '"Sql", "Pandas", NoSql')
 txt('**Machine Learning**', "Scikit-learn")
 txt('**Power BI**', "Visualization")
 
